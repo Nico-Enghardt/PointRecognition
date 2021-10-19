167fb89a9ba12dc5e0b9645633fa782070b60088
Nico Enghardt <nico.enghardt@hotmail.com>
2021-10-19 11:31:54 +0200

    Testing implemented

---
+++ readDataset.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+import skvideo.io
+
+def readDataset(path):
+
+    files = os.listdir(path);
+
+    pictures = []
+    labels = np.empty((1,3))
+
+    for file in files:
+
+        format = file[-3:]
+
+        if format=="mp4":
+            video = skvideo.io.vread(path+"/"+file)
+            for frame in video:
+                frame = frame[:,:,:2]
+                pictures.append(frame.flatten())
+            print(len(pictures))
+        
+        if format=="npy":
+            labels = np.concatenate((labels,np.load(path+"/"+file)))
+
+            ##if local:
+            ##    break;
+
+    return np.array(pictures), labels[1:,:]  # Delete first row (random inintialisation of np.empty), # Convert both arrays to numpy format
