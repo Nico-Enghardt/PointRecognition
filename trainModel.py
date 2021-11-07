96307de77425c52490c62c35fc9b87317eba0e46
Nico Enghardt <nico.enghardt@hotmail.com>
2021-11-07 18:58:15 +0100

    Increased regularization

--- trainModel.py
+++ trainModel.py
@@ -19,7 +19,7 @@ datasetName = "Huegray160"
 architecture = (4000,1000,100)
 max_epochs = 1000
 batch_size = 3000;
-regularization_factor = 0.1
+regularization_factor = 0.5
 learning_rate = 0.000001
 
 run = wandb.init(job_type="model-training", config={"epochs":0,"learning_rate":learning_rate,"batch-size":batch_size,"regularization":regularization_factor,"architecture":architecture})
